6463a5f84f9615dbf9321a64d445b4e3c7c8ee5d
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2023-05-16 16:20:10 +0200

    test - prontface and pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,62 @@
+#include <SoftwareSerial.h>
 #include <Arduino.h>
+
+#include "Init.h"
+#include "InitPins.h"
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+	Init();
+	InitPins();
+	digitalWrite(ENABLE_PIN, LOW);
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                     // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  delay(1000);                     // wait for a second
+	if (millis() % 1000 < 500)
+		digitalWrite(LED_PIN, HIGH);
+	else
+		digitalWrite(LED_PIN, LOW);
+
+	if (millis() % 1000 < 300)
+	{
+		digitalWrite(HEATER_0_PIN, HIGH);
+		digitalWrite(FAN_PIN, LOW);
+	}
+	else if (millis() % 1000 < 600)
+	{
+		digitalWrite(HEATER_0_PIN, LOW);
+		digitalWrite(FAN_PIN, LOW);
+	}
+	else
+	{
+		digitalWrite(HEATER_0_PIN, LOW);
+		digitalWrite(FAN_PIN, HIGH);
+	}
+
+	if (millis() % 10000 < 5000)
+	{
+		digitalWrite(X_DIR_PIN, HIGH);
+		digitalWrite(Y_DIR_PIN, HIGH);
+		digitalWrite(Z_DIR_PIN, HIGH);
+		digitalWrite(E_DIR_PIN, HIGH);
+	}
+	else
+	{
+		digitalWrite(X_DIR_PIN, LOW);
+		digitalWrite(Y_DIR_PIN, LOW);
+		digitalWrite(Z_DIR_PIN, LOW);
+		digitalWrite(E_DIR_PIN, LOW);
+	}
+
+	digitalWrite(X_STEP_PIN, HIGH);
+	digitalWrite(Y_STEP_PIN, HIGH);
+	digitalWrite(Z_STEP_PIN, HIGH);
+	digitalWrite(E_STEP_PIN, HIGH);
+	delay(1);
+
+	digitalWrite(X_STEP_PIN, LOW);
+	digitalWrite(Y_STEP_PIN, LOW);
+	digitalWrite(Z_STEP_PIN, LOW);
+	digitalWrite(E_STEP_PIN, LOW);
 }
---
+++ test/test_init_pins.h
@@ -0,0 +1,62 @@
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+
+#include "Init.h"
+#include "InitPins.h"
+
+void setup()
+{
+    Init();
+    InitPins();
+    digitalWrite(ENABLE_PIN, LOW);
+}
+
+void loop()
+{
+    if (millis() % 1000 < 500)
+        digitalWrite(LED_PIN, HIGH);
+    else
+        digitalWrite(LED_PIN, LOW);
+
+    if (millis() % 1000 < 300)
+    {
+        digitalWrite(HEATER_0_PIN, HIGH);
+        digitalWrite(FAN_PIN, LOW);
+    }
+    else if (millis() % 1000 < 600)
+    {
+        digitalWrite(HEATER_0_PIN, LOW);
+        digitalWrite(FAN_PIN, LOW);
+    }
+    else
+    {
+        digitalWrite(HEATER_0_PIN, LOW);
+        digitalWrite(FAN_PIN, HIGH);
+    }
+
+    if (millis() % 10000 < 5000)
+    {
+        digitalWrite(X_DIR_PIN, HIGH);
+        digitalWrite(Y_DIR_PIN, HIGH);
+        digitalWrite(Z_DIR_PIN, HIGH);
+        digitalWrite(E_DIR_PIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(X_DIR_PIN, LOW);
+        digitalWrite(Y_DIR_PIN, LOW);
+        digitalWrite(Z_DIR_PIN, LOW);
+        digitalWrite(E_DIR_PIN, LOW);
+    }
+
+    digitalWrite(X_STEP_PIN, HIGH);
+    digitalWrite(Y_STEP_PIN, HIGH);
+    digitalWrite(Z_STEP_PIN, HIGH);
+    digitalWrite(E_STEP_PIN, HIGH);
+    delay(1);
+
+    digitalWrite(X_STEP_PIN, LOW);
+    digitalWrite(Y_STEP_PIN, LOW);
+    digitalWrite(Z_STEP_PIN, LOW);
+    digitalWrite(E_STEP_PIN, LOW);
+}
